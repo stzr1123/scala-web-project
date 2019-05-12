9f449b0595cea3c32213be6583078cedfac57a90
Esteban Zacharzewski <zacha.eg@gmail.com>
2019-05-12 15:19:15 +0200

    adding the weather

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,22 +9,38 @@ import java.text.SimpleDateFormat
 import scala.concurrent.ExecutionContext.Implicits.global
 import model.SunInfo
 import play.api.libs.ws._
-
 import java.time.{ZoneId, ZonedDateTime}
 import java.time.format.DateTimeFormatter
+
+import play.api.libs.json.{JsLookupResult, JsValue}
+
+import scala.concurrent.Future
 
 
 class Application @Inject() (components: ControllerComponents, assets: Assets, ws: WSClient)
     extends AbstractController(components) {
   def index: Action[AnyContent] = Action.async {
-    val date: Date = new Date()
-    val dateStr: String = new SimpleDateFormat().format(date)
 
-    val responseF = ws.url("http://api.sunrise-sunset.org/json?" + "lat=48.1222839&lng=11.5402938&formatted=0").get()
-    responseF.map { response =>
-      val json = response.json
-      val sunriseTimeStr = (json \ "results" \ "sunrise").as[String]
-      val sunsetTimeStr = (json \ "results" \ "sunset").as[String]
+    val lat: String = "48.1222839"
+    val long: String = "11.5402938"
+    val apiKey: String = "89265e73d53a913dc87cf4ac87faf499"
+
+    val responseF: Future[WSResponse] =
+      ws.url("http://api.sunrise-sunset.org/json?" + f"lat=$lat&lng=$long&formatted=0").get
+
+    val responseWeatherF: Future[WSResponse] =
+      ws.url("http://api.openweathermap.org/data/2.5/weather?" + f"units=metric&lat=$lat&lon=$long&APPID=$apiKey").get
+
+    for {
+      responseAstro <- responseF
+      responseWeather <- responseWeatherF
+    } yield {
+      val weatherJson: JsValue = responseWeather.json
+      val temperature: Double = (weatherJson \ "main" \ "temp").as[Double]
+
+      val json: JsValue = responseAstro.json
+      val sunriseTimeStr: String = (json \ "results" \ "sunrise").as[String]
+      val sunsetTimeStr: String = (json \ "results" \ "sunset").as[String]
 
       val sunriseTime = ZonedDateTime.parse(sunriseTimeStr)
       val sunsetTime = ZonedDateTime.parse(sunsetTimeStr)
@@ -35,7 +51,7 @@ class Application @Inject() (components: ControllerComponents, assets: Assets, w
         sunsetTime.format(formatter)
       )
 
-      Ok(views.html.index(sunInfo))
+      Ok(views.html.index(sunInfo, temperature))
     }
   }
 
